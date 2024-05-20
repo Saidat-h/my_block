8e06943228e0efa7253d6c876c2c26d3d98a21cf
Abygaelle Ching <abygaelle.ching@groupe-esigelec.org>
2024-05-20 16:47:56 +0200

    Redirection Enregistrer vehicule

--- index.php
+++ index.php
@@ -79,7 +79,7 @@
     </header>
     <nav>
         <ul>
-            <li><a href="register_vehicule.php">Enregistrer un véhicule</a></li>
+            <li><a href="connexion.php">Enregistrer un véhicule</a></li>
             <li><a href="search_vehicule.php">Rechercher un véhicule</a></li>
             
         </ul>
